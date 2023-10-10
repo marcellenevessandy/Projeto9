95afd7a0b48715fb432bcca724246313703e7991
Dell <Dell@DESKTOP-20C7A40>
2023-10-09 22:05:26 -0300

    Simplificando o projeto 8 com if else

--- src/projeto8/Projeto8.java
+++ src/projeto8/Projeto8.java
@@ -11,30 +11,17 @@ public class Projeto8 {
         System.out.println("Digite um número: ");
         mes = ler.nextInt();
 
-        switch (mes) {
+        if (mes == 1 || mes == 2 || mes == 12) {
+            System.out.println("O mês informado é verão!");
+        } else if (mes == 3 || mes == 4 || mes == 5) {
+            System.out.println("O mês informado é outono!");
+        } else if (mes == 6 || mes == 7 || mes == 8) {
+            System.out.println("O mês informado é inverno!");
+        } else if (mes == 9 || mes == 10 || mes == 11) {
+            System.out.println("O mês informado é primavera!");
+        } else {
 
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("O mês informado é verão!");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("O mês informado é outono!");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("O mês informado é inverno!");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("O mês informado é primavera!");
-                break;
-            default:
-                System.out.println("O mês infomado é inválido!");
+            System.out.println("O mês infomado é inválido!");
 
         }
 
